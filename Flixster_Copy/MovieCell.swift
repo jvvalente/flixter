13cfd89ad902221e0e1d0695ff743e50dfed8ae0
jvvalente <joaovalente2010@icloud.com>
2021-02-20 11:19:54 -0500

    fist commit

---
+++ Flixster_Copy/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flixster_Copy
+//
+//  Created by Joao on 2/17/21.
+//  Copyright © 2021 Joao. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
